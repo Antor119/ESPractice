305ef2b377f8974ff75b17c881c9f7728869c862
unknown <mhantor722@gmail.com>
2024-01-13 15:52:47 +0600

    first commit

--- main23.js
+++ main23.js
@@ -1,2 +1,19 @@
 //es6 class constructor
-assdfgdgdgh
+//auto call for constructor
+//not return constructor
+//only class for only constructor 
+class myClass{
+    constructor(a,b){
+        // console.log(a+b)
+        // console.log('My First Constructor')
+        this.firstNum=a;
+        this.secondNum=b;
+    }
+
+    add(){
+        let result=this.firstNum+this.secondNum;
+        console.log(result)
+    }
+}
+var obj=new myClass(20,45);
+obj.add();
